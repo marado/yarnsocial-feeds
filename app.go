marado/yarnsocial-feeds

commit 56955d367936cc6761cac0961c81cb24e5a494a3

Fix App.GetFeeds()

app.go
@@ -122,7 +122,11 @@ func (app *App) GetFeeds() (feeds []Feed) {
 		lastModified := humanize.Time(stat.ModTime())
 
 		uri := fmt.Sprintf("%s/%s/twtxt.txt", app.conf.BaseURL, name)
-		feeds = append(feeds, Feed{Name: name, URI: uri, LastModified: lastModified})
+		if feed, ok := app.conf.Feeds[name]; ok {
+			feed.URI = uri
+			feed.LastModified = lastModified
+			feeds = append(feeds, *feed)
+		}
 	}
 
 	sort.Slice(feeds, func(i, j int) bool { return feeds[i].Name < feeds[j].Name })

commit 3d1226dff1bfb4cf45767362241bbdd65f64fab6

Fix bug in GetFeeds() that 56955d3 broke (don't modify the feed config)

app.go
@@ -122,10 +122,11 @@ func (app *App) GetFeeds() (feeds []Feed) {
 		lastModified := humanize.Time(stat.ModTime())
 
 		uri := fmt.Sprintf("%s/%s/twtxt.txt", app.conf.BaseURL, name)
-		if feed, ok := app.conf.Feeds[name]; ok {
-			feed.URI = uri
-			feed.LastModified = lastModified
-			feeds = append(feeds, *feed)
+		feed := Feed{Name: name, URI: uri, LastModified: lastModified}
+		if feedConfig, ok := app.conf.Feeds[name]; ok {
+			feed.Avatar = feedConfig.Avatar
+			feed.Description = feedConfig.Description
+			feeds = append(feeds, feed)
 		}
 	}
 